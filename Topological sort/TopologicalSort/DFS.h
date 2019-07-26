02d14627f9271a8f16adefa8209cf8badda74c2e
Dzhonson64 <31271622+Dzhonson64@users.noreply.github.com>
2019-07-26 22:10:50 +0400

    update comments

--- Topological sort/TopologicalSort/DFS.h
+++ Topological sort/TopologicalSort/DFS.h
@@ -8,18 +8,17 @@ class DFS
 {
 private:
 	Vertex * start;
-	int time;	// время входа и окончание рассмотрения вершины 
+	int time;	// РІСЂРµРјСЏ РІС…РѕРґР° Рё РѕРєРѕРЅС‡Р°РЅРёРµ СЂР°СЃСЃРјРѕС‚СЂРµРЅРёСЏ РІРµСЂС€РёРЅС‹ 
 public:
 	DFS();
 	~DFS();
-	void DFS_algoritm(DFS & graph); // обход в глубину
-	void DFS_visit(DFS & graph, Vertex & vertex); // обход в глубину
-	//queue<Vertex*> * pendingVertexes;				// очередь вершин для дальнейшего их рассмотрения !!! Должен тогда использоваться СТЕК !!!
-	list<Vertex*> * vertexes;						// список всех вершин
-	list<Vertex*>*vertexesSort;						// отсортированный топологически вершины графа
-	list<Edge> * edges;								// список рёбер
-	void addVertex(Vertex & v);						// добавление вершины
-	void addEdge(Vertex & from, Vertex & to, bool oriented = false); // добавление ребра (oriented = false - граф неориентированный, oriented = true - ориентированный)
-	list<Vertex*>* getVertexList(Vertex & vertex);	// получение списка смежности для вершины vertex
+	void DFS_algoritm(DFS & graph); // РѕР±С…РѕРґ РІ РіР»СѓР±РёРЅСѓ
+	void DFS_visit(DFS & graph, Vertex & vertex); // РѕР±С…РѕРґ РІ РіР»СѓР±РёРЅСѓ
+	//queue<Vertex*> * pendingVertexes;				// РѕС‡РµСЂРµРґСЊ РІРµСЂС€РёРЅ РґР»СЏ РґР°Р»СЊРЅРµР№С€РµРіРѕ РёС… СЂР°СЃСЃРјРѕС‚СЂРµРЅРёСЏ !!! Р”РѕР»Р¶РµРЅ С‚РѕРіРґР° РёСЃРїРѕР»СЊР·РѕРІР°С‚СЊСЃСЏ РЎРўР•Рљ !!!
+	list<Vertex*> * vertexes;						// СЃРїРёСЃРѕРє РІСЃРµС… РІРµСЂС€РёРЅ
+	list<Vertex*>*vertexesSort;						// РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ С‚РѕРїРѕР»РѕРіРёС‡РµСЃРєРё РІРµСЂС€РёРЅС‹ РіСЂР°С„Р°
+	list<Edge> * edges;								// СЃРїРёСЃРѕРє СЂС‘Р±РµСЂ
+	void addVertex(Vertex & v);						// РґРѕР±Р°РІР»РµРЅРёРµ РІРµСЂС€РёРЅС‹
+	void addEdge(Vertex & from, Vertex & to, bool oriented = false); // РґРѕР±Р°РІР»РµРЅРёРµ СЂРµР±СЂР° (oriented = false - РіСЂР°С„ РЅРµРѕСЂРёРµРЅС‚РёСЂРѕРІР°РЅРЅС‹Р№, oriented = true - РѕСЂРёРµРЅС‚РёСЂРѕРІР°РЅРЅС‹Р№)
+	list<Vertex*>* getVertexList(Vertex & vertex);	// РїРѕР»СѓС‡РµРЅРёРµ СЃРїРёСЃРєР° СЃРјРµР¶РЅРѕСЃС‚Рё РґР»СЏ РІРµСЂС€РёРЅС‹ vertex
 };
-
